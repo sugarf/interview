307849ba6467a1edf2ddf262701f33668f212ebd
sugarf <tangnaifu1234@gmail.com>
2016-09-12 15:59:40 +0800

    改进了一下，删掉了submit

--- js/effect.js
+++ js/effect.js
@@ -1,13 +1,9 @@
 document.getElementById("percent").innerHTML="1.207%";
 document.getElementById("amount").innerHTML="$842.65";
-/*function myDate(){
-   var currentDate = $("#datepicker").datepicker( "getDate" );
-   document.getElementById("date").innerHTML = currentDate;
-   alert(currentDate);
-}
-*/
-
-function getDate(){
-var d = document.getElementById("myDate").value;
-document.getElementById("date").innerHTML=d;
-}
+$( function() {
+    $( "#datepicker" ).datepicker({
+        onClose: function(date) {
+            document.getElementById("date").innerHTML = date;
+        }
+    });
+});
